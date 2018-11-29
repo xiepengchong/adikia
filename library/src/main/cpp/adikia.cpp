cdb36a07a6be3ae0723c5385fb4b4433e05c48e0
xiepengchong <xiepengchong@apusapps.com>
2018-11-29 19:44:22 +0800

    标题:动态根据版本变化花去access值，参考andhook

--- library/src/main/cpp/adikia.cpp
+++ library/src/main/cpp/adikia.cpp
@@ -4,15 +4,16 @@
 #include <time.h>
 #include <stdlib.h>
 
-static const char* ADIKIA_INIT_JAVA = "com/adikia/library/AdikiaEntry";
+static const char *ADIKIA_INIT_JAVA = "com/adikia/library/AdikiaEntry";
 
 static struct {
     jmethodID m1;
     jmethodID m2;
     size_t methodSize;
 } MethodStruct;
-static int access=3;
-
+static int access = 1;
+int size = 0;
+static bool isArt_;
 
 static long hook(JNIEnv *env, jclass type, jobject srcMethodObj, jobject destMethodObj,
                  jobject backupMethodObj) {
@@ -27,26 +28,42 @@ static long hook(JNIEnv *env, jclass type, jobject srcMethodObj, jobject destMet
 
 
 static jobject restore(JNIEnv *env, jclass type, jobject srcMethod, jlong methodPtr) {
-    int* backupMethod = reinterpret_cast<int*>(methodPtr);
-    size_t* artMethodSrc = reinterpret_cast<size_t*>(env -> FromReflectedMethod(srcMethod));
+    int *backupMethod = reinterpret_cast<int *>(methodPtr);
+    size_t *artMethodSrc = reinterpret_cast<size_t *>(env->FromReflectedMethod(srcMethod));
     memcpy(artMethodSrc, backupMethod, MethodStruct.methodSize);
-    *(artMethodSrc+access)=*(artMethodSrc+access)|0x0002;
+    *(artMethodSrc + access) = *(artMethodSrc + access) | 0x0002;
 
     return srcMethod;
 }
 
-static void init(JNIEnv* env, jclass clazz, jclass cls,jstring name,jstring sig, jboolean isstatic) {
+static void
+init(JNIEnv *env, jclass clazz, jclass cls, jstring name, jstring sig, jboolean isstatic) {
     jmethodID m;
-    const char* name_=env->GetStringUTFChars(name,0);;
-    const char* sig_=env->GetStringUTFChars(sig,0);
-    if(isstatic) {
-        m=env->GetStaticMethodID(cls, name_, sig_);
-    } else{
-        m=env->GetMethodID(cls, name_, sig_);
+    const char *name_ = env->GetStringUTFChars(name, 0);;
+    const char *sig_ = env->GetStringUTFChars(sig, 0);
+    if (isstatic) {
+        m = env->GetStaticMethodID(cls, name_, sig_);
+    } else {
+        m = env->GetMethodID(cls, name_, sig_);
     }
 
-    if(env->ExceptionCheck()){
+    if (env->ExceptionCheck()) {
         env->ExceptionClear();
+    }
+    return;
+}
+
+static void computeAccess(JNIEnv *env, jclass clazz, jobject src) {
+    size_t *mSrc = (size_t *) env->FromReflectedMethod(src);
+    size_t *com = (size_t *) env->GetStaticMethodID(env->FindClass(ADIKIA_INIT_JAVA), "compute",
+                                                    "()I");
+
+    for (int i = 0; i < size / sizeof(void *); ++i) {
+        if (*(mSrc + i) == 0x80019 && *(com + i) == 0x80009) {
+            access = i;
+            LOGE("computeAccess access=%d", access);
+            return;
+        }
     }
     return;
 }
@@ -55,33 +72,52 @@ static JNINativeMethod gMethods[] = {
         {
                 "hook",
                 "(Ljava/lang/reflect/Method;Ljava/lang/reflect/Method;Ljava/lang/reflect/Method;)J",
-                (void*)hook
+                (void *) hook
         },
         {
                 "restore",
                 "(Ljava/lang/reflect/Method;J)Ljava/lang/reflect/Method;",
-                (void*)restore
+                (void *) restore
         },
         {
                 "init",
                 "(Ljava/lang/Class;Ljava/lang/String;Ljava/lang/String;Z)V",
-                (void*)init
+                (void *) init
+        },
+        {
+                "computeAccess",
+                "(Ljava/lang/reflect/Method;)V",
+                (void *) computeAccess
         }
+
 };
 
 extern "C"
 JNIEXPORT jint JNICALL
-JNI_OnLoad(JavaVM* vm, void* reserved) {
+JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env = nullptr;
     if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK) {
         return JNI_FALSE;
     }
     jclass classEvaluateUtil = env->FindClass(ADIKIA_INIT_JAVA);
-    if(env -> RegisterNatives(classEvaluateUtil, gMethods, sizeof(gMethods)/ sizeof(gMethods[0])) < 0) {
+    if (env->RegisterNatives(classEvaluateUtil, gMethods, sizeof(gMethods) / sizeof(gMethods[0])) <
+        0) {
         return JNI_FALSE;
     }
-    MethodStruct.m1 = env -> GetStaticMethodID(classEvaluateUtil, "m1", "()V");
-    MethodStruct.m2 = env -> GetStaticMethodID(classEvaluateUtil, "m2", "()V");
-    MethodStruct.methodSize = reinterpret_cast<size_t>(MethodStruct.m2) - reinterpret_cast<size_t>(MethodStruct.m1);
+
+    MethodStruct.m1 = env->GetStaticMethodID(classEvaluateUtil, "m1", "()V");
+    MethodStruct.m2 = env->GetStaticMethodID(classEvaluateUtil, "m2", "()V");
+    size = reinterpret_cast<size_t >(MethodStruct.m2) - reinterpret_cast<size_t >(MethodStruct.m1);
+
+    jmethodID isArt = env->GetStaticMethodID(env->FindClass(ADIKIA_INIT_JAVA), "isArt", "()Z");
+
+    isArt_ = env->CallStaticBooleanMethod(env->FindClass(ADIKIA_INIT_JAVA), isArt);
+
+    if (isArt_) {
+        jmethodID com = env->GetStaticMethodID(env->FindClass(ADIKIA_INIT_JAVA), "compute", "()I");
+        size = env->CallStaticIntMethod(env->FindClass(ADIKIA_INIT_JAVA), com);
+    }
+    MethodStruct.methodSize =
+            reinterpret_cast<size_t>(MethodStruct.m2) - reinterpret_cast<size_t>(MethodStruct.m1);
     return JNI_VERSION_1_4;
 }
--- library/src/main/java/com/adikia/library/AdikiaEntry.java
+++ library/src/main/java/com/adikia/library/AdikiaEntry.java
@@ -1,11 +1,19 @@
 package com.adikia.library;
 
+import android.os.Build;
 import android.util.Log;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 class AdikiaEntry {
+
+    final public static void m1() {
+    }
+
+    final public static void m2() {
+    }
 
     private static final String TAG = "Adikia.Entry";
 
@@ -49,15 +57,14 @@ class AdikiaEntry {
         }
     }
 
-    public Object[] convert(Object[] args){
+    public Object[] convert(Object[] args) {
         int size = args.length;
         Object[] obj = new Object[size];
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             obj[i] = args[i];
         }
         return obj;
     }
-
 
 
     public Object callOrigin(Object receiver, Object args) throws InvocationTargetException, IllegalAccessException {
@@ -66,17 +73,17 @@ class AdikiaEntry {
         if (backupMethodPtr != 0) {
             Object[] argsModified = callback.beforeInvokeMethod(receiver, (Object[]) args);
             if ((result = callback.invokeMethod(receiver, argsModified)) == null) {
-                if(backupMethod.getParameterTypes().length != argsModified.length){
+                if (backupMethod.getParameterTypes().length != argsModified.length) {
                     if (AdikiaConfig.DEBUG) {
                         Log.e(TAG, "input params size wrong");
                         Object[] argsError = convert((Object[]) args);
-                        result = backupMethod.invoke(receiver,argsError);
+                        result = backupMethod.invoke(receiver, argsError);
                     }
                 } else {
-                    result = backupMethod.invoke(receiver,argsModified);
+                    result = backupMethod.invoke(receiver, argsModified);
                 }
             }
-            result = callback.afterInvokeMethod(receiver, result,args);
+            result = callback.afterInvokeMethod(receiver, result, args);
         } else {
             result = callOriginMethod(receiver, args);
         }
@@ -90,7 +97,7 @@ class AdikiaEntry {
         }
         Class returnClz = srcMethod.getReturnType();
         if (AdikiaConfig.DEBUG) {
-            Log.v(TAG, "callOriginMethod returnClz.getName()=" + returnClz.getName()+" srcMethod="+srcMethod);
+            Log.v(TAG, "callOriginMethod returnClz.getName()=" + returnClz.getName() + " srcMethod=" + srcMethod);
         }
         if (returnClz != null && "void".equalsIgnoreCase(returnClz.getName())) {
             srcMethod.invoke(receiver, args);
@@ -105,17 +112,66 @@ class AdikiaEntry {
 
     private static native Method restore(Method src, long backup);
 
-    public static native void init(Class cls,String name,String sig,boolean istatic);
+    public static native void init(Class cls, String name, String sig, boolean istatic);
 
-    public static void m1() {
-    }
+    private static native void computeAccess(Method m);
 
-    public static void m2() {
+
+    public static int compute() {
+        if (!isArt()) {
+            return 0;
+        }
+        if (Build.VERSION.SDK_INT >= 26) {
+            try {
+                Class<?> abstractMethodClass = Class.forName("java.lang.reflect.Executable");
+                Field artMethodField = abstractMethodClass.getDeclaredField("artMethod");
+                artMethodField.setAccessible(true);
+                Method m1 = AdikiaEntry.class.getDeclaredMethod("m1");
+                Method m2 = AdikiaEntry.class.getDeclaredMethod("m2");
+                int res = (int) (artMethodField.getLong(m1) - artMethodField.getLong(m2));
+                return Math.abs(res);
+            } catch (Throwable e) {
+                Log.d(TAG, "", e);
+            }
+        } else if (Build.VERSION.SDK_INT >= 23) {
+            try {
+                Class<?> abstractMethodClass = Class.forName("java.lang.reflect.AbstractMethod");
+                Field artMethodField = abstractMethodClass.getDeclaredField("artMethod");
+                artMethodField.setAccessible(true);
+                Method m1 = AdikiaEntry.class.getDeclaredMethod("m1");
+                Method m2 = AdikiaEntry.class.getDeclaredMethod("m2");
+                int res = (int) (artMethodField.getLong(m1) - artMethodField.getLong(m2));
+                return Math.abs(res);
+            } catch (Throwable e) {
+                Log.d(TAG, "", e);
+            }
+        } else {
+            try {
+                Class<?> artMethodClass = Class.forName("java.lang.reflect.ArtMethod");
+                Field f_objectSize = Class.class.getDeclaredField("objectSize");
+                f_objectSize.setAccessible(true);
+                return f_objectSize.getInt(artMethodClass);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return 0;
     }
 
     static {
         System.loadLibrary("adikia");
+        if (isArt()) {
+            try {
+                computeAccess(AdikiaEntry.class.getDeclaredMethod("m1"));
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-
+    public static boolean isArt() {
+        final String vmVersion = System.getProperty("java.vm.version");
+        boolean isArt = vmVersion != null && vmVersion.startsWith("2");
+        return isArt;
+    }
 }
